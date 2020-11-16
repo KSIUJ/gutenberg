8248f03a4841f2457983cd7a5682aa7cb101a049
Adam Pardyl <adam@pardyl.com>
2020-11-16 16:37:42 +0100

    Add job tracking, rework file conversion, process jobs in async

--- sandbox.sh
+++ sandbox.sh
@@ -6,8 +6,6 @@ bwrap --ro-bind / / \
       --tmpfs /home \
       --tmpfs /run \
       --tmpfs /tmp \
-      --tmpfs /root/.config/libreoffice \
-      --tmpfs /var/www/.cache \
       --bind "$dir" "$dir" \
       --proc /proc \
       --dev /dev \
@@ -15,3 +13,6 @@ bwrap --ro-bind / / \
       --unshare-pid \
       --unshare-net \
       "$@"
+
+#      --tmpfs /root/.config/libreoffice \
+#      --tmpfs /var/www/.cache \
