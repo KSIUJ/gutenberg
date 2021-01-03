6c51e12c414ea90451c562612c324f137363eef4
Adam Pardyl <adam@pardyl.com>
2021-01-03 01:25:47 +0100

    Implement PWA functionality

--- printing/urls.py
+++ printing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 
-from printing.views import webapp
+from printing.views import webapp, service_worker
 
 urlpatterns = [
     path('', webapp, name='print'),
     re_path('^job/.*', webapp, name='print'),
+    path('service-worker.js', service_worker)
 ]
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -6,6 +6,7 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
