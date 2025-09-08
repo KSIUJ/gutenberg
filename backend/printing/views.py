77357e4d23ec19b65ebb84a4257ad8e83940e30f
Dominik <29484605+dominik-korsa@users.noreply.github.com>
2025-09-09 00:50:07 +0200

    Update comment for the `webapp_login` view

--- backend/printing/views.py
+++ backend/printing/views.py
@@ -17,8 +17,7 @@ def webapp_require_auth(request):
 
 
 # This method does not redirect to / or the ?next route if the user is already
-# signed in, it is the job of the GutenbergLoginView, a subclass of
-# BaseLoginView from the docker_ksi_auth package.
+# signed in, it is the job of the `OidcLoginView` from the `ksi-oidc-django` package.
 def webapp_login(request):
     # Serve webpack-built SPA.
     return serve(request, "200.html", settings.GUTENBERG_SPA_HTML_DIR)
