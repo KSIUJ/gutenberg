8eb43584cb229e93bd1fe55f1298d238884367f8
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-09-10 10:54:09 +0200

    Revert overriding Django error pages

--- backend/api/exceptions.py
+++ backend/api/exceptions.py
@@ -1,8 +1,21 @@
 from django.core.exceptions import PermissionDenied as DjangoPermissionDenied
-from django.http import Http404 as DjangoHttp404
-from rest_framework.exceptions import ValidationError, APIException, \
-    NotAuthenticated, PermissionDenied, AuthenticationFailed, NotFound
-from rest_framework.response import Response
+from django.http import Http404 as DjangoHttp404, JsonResponse
+from rest_framework import exceptions
+
+
+def _get_exception_kind(exc):
+    if isinstance(exc, exceptions.ValidationError):
+        return 'ValidationError'
+    elif isinstance(exc, exceptions.NotAuthenticated):
+        return 'NotAuthenticated'
+    elif isinstance(exc, exceptions.AuthenticationFailed):
+        return 'AuthenticationFailed'
+    elif isinstance(exc, exceptions.PermissionDenied):
+        return 'PermissionDenied'
+    else:
+        # More values for `kind` might be added in the future for existing or new error types.
+        # Clients should not depend on receiving the `Other` kind to test for a specific error type.
+        return "Other"
 
 
 def custom_exception_handler(exc, context):
@@ -21,58 +34,52 @@ def custom_exception_handler(exc, context):
     # Convert these Django errors to instances of `APIException` before handling them.
     # This is the same behavior as the default DRF exception handler.
     if isinstance(exc, DjangoHttp404):
-        exc = NotFound(*exc.args)
+        exc = exceptions.NotFound(*exc.args)
     elif isinstance(exc, DjangoPermissionDenied):
-        exc = PermissionDenied(*exc.args)
+        exc = exceptions.PermissionDenied(*exc.args)
 
-    if not isinstance(exc, APIException):
-        # Returns the generic Django 500 response.
-        # https://www.django-rest-framework.org/api-guide/exceptions/#generic-error-views
-        # https://docs.djangoproject.com/en/5.2/topics/http/views/#customizing-error-views
-        return None
+    if not isinstance(exc, exceptions.APIException):
+        # Returns a generic 500 response to avoid leaking internal data unintentionally.
+        return JsonResponse(
+            {
+                'kind': 'Other',
+                'message': 'An internal server error occurred.',
+                'detail': None,
+            },
+            status=500,
+        )
 
-    def kind_type(exc):
-        if isinstance(exc, ValidationError):
-            return 'ValidationError'
-        elif isinstance(exc, NotAuthenticated):
-            return 'NotAuthenticated'
-        elif isinstance(exc, AuthenticationFailed):
-            return 'AuthenticationFailed'
-        elif isinstance(exc, PermissionDenied):
-            return 'PermissionDenied'
-        else:
-            # More values for `kind` might be added in the future for existing or new error types.
-            # Clients should not depend on receiving the `Other` kind to test for a specific error type.
-            return "Other"
-
-    response = Response()
-    response.status_code = getattr(exc, 'status_code', 500)
+    status_code = getattr(exc, 'status_code', 500)
     detail = getattr(exc, 'detail', "An error occurred.")
     additional_info = getattr(exc, 'additional_info', None)
-    if isinstance(exc, ValidationError): #validation error format
-        response.data = {
-            # `ValidationError` is different from other standard subclasses of `ApiError`,
-            # its `detail` field might contain multiple errors - the `ValidationError` kind was added to support it.
-            # Error responses with the `kind` of `ValidationError` have the extra `errors` field.
-            'kind': kind_type(exc),
+
+    if isinstance(exc, exceptions.ValidationError): # validation error format
+        # `ValidationError` is different from other standard subclasses of `ApiError`,
+        # its `detail` field might contain multiple errors - the `ValidationError` kind was added to support it.
+        # Error responses with the `kind` of `ValidationError` have the extra `errors` field.
+        data = {
+            'kind': _get_exception_kind(exc),
             'message': 'The server received an invalid request',
             'detail': additional_info,
             'errors': detail
         }
-    else: #standard error format
-        response.data = {
-            'kind': kind_type(exc),
+    else: # standard error format
+        data = {
+            'kind': _get_exception_kind(exc),
             'message': detail,
             'detail': additional_info
         }
-    return response
 
-class UnsupportedDocument(APIException):
+    return JsonResponse(data, status=status_code)
+
+
+class UnsupportedDocument(exceptions.APIException):
     status_code = 422
     default_detail = 'The provided document is not supported.'
     default_code = 'unsupported_document'
 
-class InvalidStatus(APIException):
+
+class InvalidStatus(exceptions.APIException):
     status_code = 422
     default_detail =  'Invalid Job status for this request.'
     default_code = 'invalid_status'
