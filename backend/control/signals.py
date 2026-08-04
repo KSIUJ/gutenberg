46137e324e5fe2de85b75cb6fa6a5bbcf560e0ef
Ser-Zar <ser-zar@users.noreply.github.com>
2026-08-04 13:27:00 +0200

    Print Preview - API - final version

--- backend/control/signals.py
+++ backend/control/signals.py
@@ -1,7 +1,7 @@
 """Signal handlers for control models."""
 
-from django.db.models.signals import post_delete
 from django.db import transaction
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
 from control.models import JobArtefact, PrintPreviewPage
@@ -9,12 +9,8 @@ from control.models import JobArtefact, PrintPreviewPage
 
 @receiver(post_delete, sender=JobArtefact)
 def delete_job_artefact_file(sender, instance, **kwargs):
-# Deleting a model instance does not automatically delete files referenced by `FileField`s in the model.
-# These handlers automatically delete files after the model instance is deleted.
-# TODO: Are the signals sent after a database commit or just after calling `delete`? If it's the latter, a rollbacked transaction will lead to missing files.
-
-@receiver(post_delete, sender=JobArtefact)
-def delete_job_artefact_file(sender, instance, **kwargs):
+    # FileField storage is outside the DB transaction, so delete the blob only
+    # after the row deletion commits successfully.
     if instance.file and instance.file.name:
         file = instance.file
         transaction.on_commit(lambda: file.delete(save=False))
@@ -22,6 +18,7 @@ def delete_job_artefact_file(sender, instance, **kwargs):
 
 @receiver(post_delete, sender=PrintPreviewPage)
 def delete_preview_page_file(sender, instance, **kwargs):
+    # Empty FileFields have no stored blob, so skip them.
     if instance.image and instance.image.name:
         image = instance.image
         transaction.on_commit(lambda: image.delete(save=False))
